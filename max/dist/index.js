497910a6a5fcfcc8505cc3654d4dab37c0d88506
MaximeCaillaud <maxime.caillaud93@gmail.com>
2023-07-11 14:53:13 +0200

    Maj importante

--- max/dist/index.js
+++ max/dist/index.js
@@ -1,32 +1,12 @@
-const myAtropos1 = Atropos({
-    el: '.my-atropos1',
-    shadow: false
-});
-const myAtropos2 = Atropos({
-    el: '.my-atropos2',
-    shadow: false,
-    rotate: true,
-    rotateTouch: false,
-    activeOffset: 150
-});
-const myAtropos3 = Atropos({
-    el: '.my-atropos3',
-    shadow: true,
-    shadowOffset: 75,
-    shadowScale: 0.6,
-    rotateXMax: 35,
-    rotateYMax: 35,
-    rotateXInvert: true,
-    rotateYInvert: true,
-    highlight: false
-});
+
 
 var banner = document.getElementsByClassName('banner');
 new simpleParallax(banner, {
     scale: 2,
     delay: 1.5,
     overflow: true,
-    direction: 'up'
+    direction: 'up',
+    transition: 'cubic-bezier(0,0,0,1)'
 });
 
 AOS.init();
